0c58be8050297fd9540034149e350d95b5e33140
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2020-04-29 23:38:52 -0600

    actual implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState, useMemo } from "react";
+import combinatorics from "js-combinatorics";
+import "./App.css";
 
 function App() {
+  const [flat, setFlat] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
+  const [difficulty, setDifficulty] = useState(0);
+  const setFlatFromEvent = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setFlat(+e.target.value),
+    []
+  );
+  const setAccuracyFromEvent = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setAccuracy(+e.target.value),
+    []
+  );
+  const setDifficultyFromEvent = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => setDifficulty(+e.target.value),
+    []
+  );
+  const probability = useMemo(() => {
+    const d20Values = Array.from(Array(20).keys()).map((i) => i + 1);
+    const accMod = accuracy - difficulty;
+    const sign = accuracy > difficulty ? 1 : -1;
+    const accValueArrays = Array.from(Array(Math.abs(accMod)).keys()).map((i) =>
+      Array.from(Array(6).keys()).map((i) => i + 1)
+    );
+
+    const results = combinatorics
+      .cartesianProduct(d20Values, ...accValueArrays)
+      .map(([base, ...mods]) => {
+        return (
+          base +
+          flat +
+          (mods.length ? Math.max(...(mods as number[])) * sign : 0)
+        );
+      });
+
+    return d20Values.map(
+      (v) =>
+        (results.filter((r: number) => r >= v).length / results.length) * 100.0
+    );
+  }, [flat, accuracy, difficulty]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label>
+          Flat Bonus
+          <input type="number" onChange={setFlatFromEvent} value={flat} />
+        </label>
+        <label>
+          Accuracy
+          <input
+            type="number"
+            onChange={setAccuracyFromEvent}
+            value={accuracy}
+          />
+        </label>
+        <label>
+          Difficulty
+          <input
+            type="number"
+            onChange={setDifficultyFromEvent}
+            value={difficulty}
+          />
+        </label>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Target</th>
+            <th>Probability</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(Array(20).keys()).map((i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{probability[i].toFixed(1)}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
