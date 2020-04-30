e0b380d0d4296bee7668a02e7d25ee58207918f0
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2020-04-29 23:45:25 -0600

    Updates

---
+++ precache-manifest.2f62a4fa6053913df576413271c0a784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fb3bacb4a7b2df91211625a9a21fc1",
+    "url": "/lancer-dice/index.html"
+  },
+  {
+    "revision": "75c2524c044ae2d54d12",
+    "url": "/lancer-dice/static/css/main.176f753f.chunk.css"
+  },
+  {
+    "revision": "0851a9a826851b4111ad",
+    "url": "/lancer-dice/static/js/2.9f5729ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lancer-dice/static/js/2.9f5729ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c2524c044ae2d54d12",
+    "url": "/lancer-dice/static/js/main.85436432.chunk.js"
+  },
+  {
+    "revision": "7452368326dc70ca6fce",
+    "url": "/lancer-dice/static/js/runtime-main.0628c183.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d20ed14c01600f8ce232acac4a56c787.js"
+  "/lancer-dice/precache-manifest.2f62a4fa6053913df576413271c0a784.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lancer-dice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
