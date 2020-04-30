437529bd6ccc00811c08d4f359f3decb8ebd8517
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2020-04-29 23:42:27 -0600

    Updates

---
+++ precache-manifest.d20ed14c01600f8ce232acac4a56c787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e18538cb6283f361efaed838993c90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dccab49ac04fb3c494fc",
+    "url": "/static/css/main.176f753f.chunk.css"
+  },
+  {
+    "revision": "0851a9a826851b4111ad",
+    "url": "/static/js/2.9f5729ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f5729ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccab49ac04fb3c494fc",
+    "url": "/static/js/main.9e1af185.chunk.js"
+  },
+  {
+    "revision": "4f7cf31793120dd22efb",
+    "url": "/static/js/runtime-main.de636ac0.js"
+  }
+]);
